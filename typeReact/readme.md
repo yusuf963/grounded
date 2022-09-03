bca5ae12e3d3e18010597321ea3f95ab355643ad
yusuf963 <mohammad963yusuf@gmail.com>
2022-09-03 21:23:06 +0100

    add react libs to the project

--- typeReact/readme.md
+++ typeReact/readme.md
@@ -1,3 +1,12 @@
 `npm install -D webpack webpack-cli`
 `npm install -D html-webpack-plugin` serve html
 `npm install -D webpack-dev-server ` hot reload
+`npm install -D typescript ts-loader` for webpack
+`npm i -D @babel/core @babel/preset-env @babel/preset-typescript`
+`npm install babel-loader`
+`npm install -D @babel/plugin-proposal-class-properties`
+
+## React
+`npm i react react-dom`
+`npm i -D @types/react @types/react-dom`
+`npm i -D @babel/preset-react`
--- typeReact/webpack.config.js
+++ typeReact/webpack.config.js
@@ -1,10 +1,23 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  entry: './src/index.tsx',
+  resolve: {
+    extensions: ['.js', '.ts', '.tsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
       filename: 'index.html',
-    })
-  ]
-}
+    }),
+  ],
+};
